01fbbe27b1c3d27ddc72a1d9f8534cd117ffb2e2
Dale <dalebittner274@gmail.com>
2023-10-25 12:11:08 -0400

    Original

---
+++ encoder.py
@@ -0,0 +1,27 @@
+def encode(number):
+    original = str(number)
+    new = ""
+    for char in original:
+        new += str(int(char) + 3)
+    return newg
+
+def main():
+    running = True
+    while running:
+        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit\n")
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter your password to encode: ")
+            password = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif option == '2':
+            try:
+                print(f"The encoded password is {password} and the original password is {decode(password)}.\n")
+            except:
+                print("No decoder function found or no password to decode")
+        elif option == '3':
+            quit()
+
+if __name__ == "__main__":
+    main()
